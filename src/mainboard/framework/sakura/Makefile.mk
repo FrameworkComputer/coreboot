cd2f338ac04db5926d82a2aa15a6ef76b12cc6b3
Daniel Schaefer <dhs@frame.work>
2026-06-22 13:47:02 +0800

    mb/framework: Add Kconfig options to embed PD FW binaries
    Change-Id: Iab2118c636e3d60956be816b174420b7005c6574
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/mainboard/framework/sakura/Makefile.mk
+++ src/mainboard/framework/sakura/Makefile.mk
@@ -14,3 +14,16 @@ ramstage-y += hda_verb.c
 ramstage-y += ramstage.c
 
 smm-$(CONFIG_HAVE_SMI_HANDLER) += smihandler.c
+
+# Embed the USB-C PD controller firmware into the PD1/PD2 FMAP regions (at the
+# vendor's fixed BIOS-region offsets, see board.fmd). Each blob is optional:
+# an unset region is left empty so flashing preserves what's already on chip.
+ifneq ($(CONFIG_FRAMEWORK_PD1_FW_FILE),"")
+$(call add_intermediate, add_pd1_fw)
+	$(CBFSTOOL) $< write -r PD1 -f $(CONFIG_FRAMEWORK_PD1_FW_FILE) --fill-upward
+endif
+
+ifneq ($(CONFIG_FRAMEWORK_PD2_FW_FILE),"")
+$(call add_intermediate, add_pd2_fw)
+	$(CBFSTOOL) $< write -r PD2 -f $(CONFIG_FRAMEWORK_PD2_FW_FILE) --fill-upward
+endif
