2ea626fcae4718b9a9f6daec4dea54ecacfec09b
Daniel Schaefer <dhs@frame.work>
2026-09-25 17:29:28 +0200

    mb/framework/sakura: Fix RAM swizzle
    I have only been using 32GB LPCAMM modules and it has been fine so far.
    Now I tried 64GB and it wouldn't boot.
    
    Thanks Dustin for figuring out that with the 64GB module one channel
    wasn't working and disabling it would make the system boot.
    
    The DQ map matches Intel's PTL-UH "CAMM BEP" reference table. It is not
    the T3 RVP table (camm_t3_mem_config in ptlrvp): channels 3, 5, 6 and 7
    differ. The DQS byte map is the same in all three.
    
    TEST=Successful boot on sakura DVT1 with 64GB LPCAMM2 module
    
    Tested-by: Dustin L. Howett <dustin@howett.net>
    Change-Id: I719a71595bcd4d4460d926f045baa414185c26aa
    Signed-off-by: Daniel Schaefer <dhs@frame.work>

--- src/mainboard/framework/sakura/romstage.c
+++ src/mainboard/framework/sakura/romstage.c
@@ -4,7 +4,11 @@
 #include <soc/romstage.h>
 
 /*
- * LPDDR5x LPCAMM2 module, use same config as camm_t3_mem_config on ptl_rvp
+ * LPDDR5x LPCAMM2 module.
+ *
+ * The DQ/DQS maps come from the schematic: sheet 9 ties SoC balls to DDR_Mx nets,
+ * and sheet 24 routes those nets to the LPCAMM2 connector pins.
+ * Each entry is the connector DQ pin that the SoC DQ bit is wired to.
  */
 void mainboard_memory_init_params(FSPM_UPD *mupd)
 {
@@ -25,24 +29,24 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 				.dq1 = { 12, 13, 15, 14,  8, 11,  9, 10 },
 			},
 			.ddr3 = {
-				.dq0 = { 14, 15, 12, 13, 10,  8, 11,  9 },
-				.dq1 = {  4,  6,  7,  5,  1,  3,  0,  2 },
+				.dq0 = { 14, 15, 12, 13, 10,  8,  9, 11 },
+				.dq1 = {  4,  6,  7,  5,  1,  3,  2,  0 },
 			},
 			.ddr4 = {
 				.dq0 = {  3,  0,  2,  1,  6,  7,  4,  5 },
 				.dq1 = { 13, 12, 15, 14,  8, 10, 11,  9 },
 			},
 			.ddr5 = {
-				.dq0 = { 10,  8, 11,  9, 13, 15, 12, 14 },
+				.dq0 = { 10,  8, 11,  9, 13, 15, 14, 12 },
 				.dq1 = {  2,  1,  3,  0,  7,  6,  5,  4 },
 			},
 			.ddr6 = {
-				.dq0 = {  3,  1,  2,  0,  5,  7,  4,  6 },
-				.dq1 = { 12, 14, 15, 13,  9, 10, 11,  8 },
+				.dq0 = {  3,  1,  2,  0,  6,  7,  4,  5 },
+				.dq1 = { 12, 13, 15, 14,  9, 10, 11,  8 },
 			},
 			.ddr7 = {
-				.dq0 = {  8,  9, 10, 11, 12, 13, 14, 15 },
-				.dq1 = {  5,  6,  7,  4,  2,  1,  3,  0 },
+				.dq0 = {  9,  8, 10, 11, 12, 15, 14, 13 },
+				.dq1 = {  5,  6,  7,  4,  0,  1,  3,  2 },
 			},
 		},
 
@@ -54,7 +58,7 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 			.ddr4 = { .dqs0 = 0, .dqs1 = 1 },
 			.ddr5 = { .dqs0 = 1, .dqs1 = 0 },
 			.ddr6 = { .dqs0 = 0, .dqs1 = 1 },
-			.ddr7 = { .dqs0 = 1, .dqs1 = 0 }
+			.ddr7 = { .dqs0 = 1, .dqs1 = 0 },
 		},
 
 		.ect = true, /* Early Command Training */
@@ -62,6 +66,7 @@ void mainboard_memory_init_params(FSPM_UPD *mupd)
 		.user_bd = BOARD_TYPE_ULT_ULX,
 
 		.lp5x_config = {
+			/* CA pins: channels 0/2/4/6 descending, 1/3/5/7 ascending */
 			.ccc_config = 0x55,
 		},
 	};
